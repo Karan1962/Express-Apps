82ed0d6a46d4e914f041d6a1001ce567550269fd
karan <karansawan5050@gmail.com>
2023-09-30 20:38:24 +0530

    debug routing

--- Blog-Website/app.js
+++ Blog-Website/app.js
@@ -1,10 +1,9 @@
-//jshint esversion:6
-import express from "express"
-import bodyParser from "body-parser"
+import express from "express";
+import bodyParser from "body-parser";
 import ejs from "ejs";
 
 const homeStartingContent =
-"Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
   "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
@@ -14,7 +13,7 @@ const app = express();
 
 let arrs = [];
 
-app.set("view engine", ejs);
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -35,34 +34,48 @@ app.get("/post", function (req, res) {
   res.render("email");
 });
 
-app.get("/posts",(req,res)=>{
-  console.log("triggered")
-  res.render('posts',{arrs:arrs})
-})
+app.get("/posts", (req, res) => {
+  console.log("triggered");
+  res.render("posts", { arrs: arrs });
+});
 app.get("/post/:postName", function (req, res) {
   const requestedTitle = req.params.postName;
-  
+  console.log(arrs);
+  let found = false;
+
   arrs.forEach(function (arr) {
     const storedTitle = arr.title;
-    
+
     if (storedTitle === requestedTitle) {
       console.log("matched found");
-      res.render('certain',{arr:arr})
+      res.render("certain", { arr: arr });
+      found = true;
     }
   });
+
+  if (!found) {
+    res.status(404).send("Post not found");
+  }
 });
 
 app.get("/:postName", function (req, res) {
   const requestedTitle = req.params.postName;
 
+  let found = false;
+
   arrs.forEach(function (arr) {
     const storedTitle = arr.compose;
 
     if (storedTitle === requestedTitle) {
-      console.log("matched found");
-      res.render('linkpost',{arr:arr})
+      console.log("Match found");
+      res.render("linkpost", { arr: arr });
+      found = true;
     }
   });
+
+  if (!found) {
+    res.status(404).send("Post not found");
+  }
 });
 
 app.post("/post", function (req, res) {
@@ -76,11 +89,6 @@ app.post("/post", function (req, res) {
   arrs.push(post);
   res.redirect("/");
 });
-
-
-
-
-
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
